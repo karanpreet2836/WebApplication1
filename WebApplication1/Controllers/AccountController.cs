949e2eecb7c67ddd300f408ad69df0321f41ac3d
big bird <big bird@DESKTOP-K4AE38A>
2021-08-30 19:50:38 +0530

    Add project files.

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Web.Security;
+
+
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using(var context = new OfficeEntities1())
+            {
+                bool isValid = context.Logins.Any(x => x.UserName == model.UserName && x.Password == model.Password);
+                if(isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Candidates");
+                }
+                ModelState.AddModelError("","Invalid Username and Password");
+                return View();
+            }
+          
+        }
+
+        [HttpGet]
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(Login model)
+        {
+            using(var context = new OfficeEntities1())
+            {
+                context.Logins.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ WebApplication1/Controllers/CandidatesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    public class CandidatesController : Controller
+    {
+        private OfficeEntities1 db = new OfficeEntities1();
+
+        // GET: Candidates
+        public ActionResult Index()
+        {
+            return View(db.Candidates.ToList());
+        }
+
+        // GET: Candidates/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Candidate candidate = db.Candidates.Find(id);
+            if (candidate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidate);
+        }
+
+        // GET: Candidates/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Candidates/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CandidateID,Email,Password,FirstName,LastName")] Candidate candidate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Candidates.Add(candidate);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(candidate);
+        }
+
+        // GET: Candidates/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Candidate candidate = db.Candidates.Find(id);
+            if (candidate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidate);
+        }
+
+        // POST: Candidates/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CandidateID,Email,Password,FirstName,LastName")] Candidate candidate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(candidate).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(candidate);
+        }
+
+        // GET: Candidates/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Candidate candidate = db.Candidates.Find(id);
+            if (candidate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidate);
+        }
+
+        // POST: Candidates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Candidate candidate = db.Candidates.Find(id);
+            db.Candidates.Remove(candidate);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
